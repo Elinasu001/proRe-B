4c63a96b7e178b88c393b7af699cf7be097d22db
ahavatyeshua <ani.ana.yungi@gmail.com>
2026-01-19 14:38:56 +0900

    refactor: 매퍼 주석에 return 구문 보완

--- proResponse/src/main/java/com/kh/even/back/member/model/mapper/MemberMapper.java
+++ proResponse/src/main/java/com/kh/even/back/member/model/mapper/MemberMapper.java
@@ -11,20 +11,21 @@ public interface MemberMapper {
 	/**
 	 * 이메일 중복확인
 	 * @param email
-	 * @return
+	 * @return DB에 존재하는 동일한 이메일의 개수를 반환해줍니다.
 	 */
 	int countByEmail(String email);
 	
 	/**
 	 * 회원가입
 	 * @param member
+	 * @return DB에 새로 추가된 행의 개수를 반환해줍니다.
 	 */
 	int signUp(MemberVO member);
 	
 	/**
 	 * 회원 위도/경도 추가
 	 * @param member
-	 * @return
+	 * @return DB에 새로 추가된 행의 개수를 반환해줍니다.
 	 */
 	int saveLocation(MemberVO member);
 

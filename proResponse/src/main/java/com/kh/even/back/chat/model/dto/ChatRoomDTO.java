8b32c49538ec0d43de37cff340b2e1606627734d
suelina001 <elinaen001@gmail.com>
2026-01-23 07:04:40 +0900

    refactor : 정리

--- proResponse/src/main/java/com/kh/even/back/chat/model/dto/ChatRoomDTO.java
+++ proResponse/src/main/java/com/kh/even/back/chat/model/dto/ChatRoomDTO.java
@@ -15,10 +15,10 @@ import lombok.ToString;
 @NoArgsConstructor 
 @ToString
 public class ChatRoomDTO {
-    private Long roomNo;
-    private String status;
-    private LocalDateTime createDate;
-    private Long estimateNo;
-    private Long userNo; //
-    private String userName; //
+    private Long roomNo;              // 채팅방 번호 (PK)
+    private String status;            // 채팅방 상태
+    private LocalDateTime createDate; // 생성일
+    private Long estimateNo;          // 견적 번호 (FK)
+    private Long userNo;              // 회원 번호 (FK)
+    private String userName;          // 회원 이름
 }
--- proResponse/src/main/java/com/kh/even/back/chat/model/vo/ChatAttachmentVO.java
+++ proResponse/src/main/java/com/kh/even/back/chat/model/vo/ChatAttachmentVO.java
@@ -11,7 +11,7 @@ public class ChatAttachmentVO {
     private Long fileNo;				// 파일번호 (PK)
 	private String originName;			// 원본 파일명
 	private String filePath;			// 파일 경로
-	private LocalDateTime uploadDate;	        // 파일 업로드 날짜
+	private LocalDateTime uploadDate;   // 파일 업로드 날짜
 	private String status;            	// STATUS (Y/N)
     private Long messageNo;             // 메시지 번호 (FK)
 }

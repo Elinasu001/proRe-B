d4ea7da2accee6882695351ea83e6a906c57564a
ahavatyeshua <ani.ana.yungi@gmail.com>
2026-01-22 16:48:32 +0900

    feat : 로그아웃

---
+++ proResponse/src/main/java/com/kh/even/back/member/model/dto/MemberLogoutDTO.java
@@ -0,0 +1,17 @@
+package com.kh.even.back.member.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberLogoutDTO {
+	
+	private String refreshToken;
+	private Long userNo;
+
+}

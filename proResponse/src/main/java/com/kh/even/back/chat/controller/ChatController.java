bd7b519e028783f234c2b1f83536336e85c36875
suelina <suelina001@gmail.com>
2026-01-23 14:58:51 +0900

    refactor: import 삭제

--- proResponse/src/main/java/com/kh/even/back/chat/controller/ChatController.java
+++ proResponse/src/main/java/com/kh/even/back/chat/controller/ChatController.java
@@ -1,8 +1,6 @@
 package com.kh.even.back.chat.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,20 +11,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.even.back.auth.model.vo.CustomUserDetails;
 import com.kh.even.back.chat.model.dto.ChatMessageDTO;
-import com.kh.even.back.chat.model.dto.ChatRoomDTO;
 import com.kh.even.back.chat.model.service.ChatService;
 import com.kh.even.back.chat.model.vo.ChatMessageVO;
 import com.kh.even.back.chat.model.vo.ChatRoomVO;
 import com.kh.even.back.common.ResponseData;
-import com.kh.even.back.review.model.vo.ReviewVO;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j

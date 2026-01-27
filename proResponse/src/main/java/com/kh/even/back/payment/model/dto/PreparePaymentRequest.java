7d6043980982a1e822a1a76bde42be79f9aafba1
suelina <suelina001@gmail.com>
2026-01-27 11:34:16 +0900

    feat: 결제 기능 작업중

---
+++ proResponse/src/main/java/com/kh/even/back/payment/model/dto/PreparePaymentRequest.java
@@ -0,0 +1,28 @@
+package com.kh.even.back.payment.model.dto;
+
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PreparePaymentRequest {
+    @NotNull(message = "견적 번호는 필수입니다")
+    private Long estimateNo;
+
+    @NotNull(message = "채팅방 번호는 필수입니다")
+    private Long roomNo;
+
+    @NotNull(message = "결제 금액은 필수입니다")
+    @Positive(message = "결제 금액은 0보다 커야 합니다")
+    private Integer amount;
+
+}
--- proResponse/src/main/java/com/kh/even/back/payment/model/service/PaymentService.java
+++ proResponse/src/main/java/com/kh/even/back/payment/model/service/PaymentService.java
@@ -1,12 +1,22 @@
 package com.kh.even.back.payment.model.service;
 
 import com.kh.even.back.payment.model.dto.PaymentDetailDTO;
+import com.kh.even.back.payment.model.dto.PreparePaymentRequest;
 
 public interface PaymentService {
 
     /**
      * 채팅방 기준 결제 정보 조회
      */
-    PaymentDetailDTO getPayment(Long roomNo, Long userNo);
+    PaymentDetailDTO getPayment(Long estimateNo, Long userNo);
 
+    /**
+     * 결제 준비
+     */
+    PaymentDetailDTO preparePayment(PreparePaymentRequest request, Long userNo);
+
+    /**
+     * 결제 완료 검증 및 처리
+     */
+    void verifyAndCompletePayment(String impUid, String merchantUid);
 }
